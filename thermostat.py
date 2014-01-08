b2b1c47b11a2cae14d998ad25d3e19a179da68c5
Darren Gibbard <dalgibbard@gmail.com>
2014-01-08 07:33:11 +0000

    Updated various issues; better value handling, correct computations of floats and various display fixes. Plus, on error, heater is switched off

--- thermostat.py
+++ thermostat.py
@@ -102,14 +102,14 @@ try:
         msg = "Temp Max: " + str(TSET) + str(TEMP_SCALE)
         lcd.lcd_display_string(msg, 3)
 
-        print color.HEADER + ( "Settings:\n" + "Time : " + str(HOUR) + ":" + str(MINUTE) + "\n" + "Temperature min: " + str(TSET - TMARG) + str(TEMP_SCALE) + "\n" + "Temperature max: " + str(TSET) + str(TEMP_SCALE) + "\n") + color.ENDC
+        print color.HEADER + ( "Settings:\n" + "Time : " + str(HOUR).zfill(2) + ":" + str(MINUTE).zfill(2) + "\n" + "Temperature min: " + str(TSET - TMARG) + str(TEMP_SCALE) + "\n" + "Temperature max: " + str(TSET) + str(TEMP_SCALE) + "\n") + color.ENDC
        
    
         #Reading curent temperature and making sure it's a number
         tmpinit = read_temp()
-	tmp = format(tmpinit, '.3f').zfill(6)
+	tmp = float(format(tmpinit, '.3f').zfill(6))
         try:
-            float(tmp)
+            int(tmp)
         except:
             msg = "Temperature value is not a number"
             lcd.lcd_display_string(msg, 4)
@@ -119,57 +119,59 @@ try:
    
         #Making decision weather to switch on/off
         #tmp >=tset - switching off
-        if tmp >= TSET and (STATE==None or STATE==True):
+        if float(tmp) >= float(TSET) and (STATE==None or STATE==True):
             STATE = False
             sockets("off", YELLOW)
-            msg = "TEMP " + str(tmp) + str(TEMP_SCALE) + "- SW OFF"
+            msg = "TEMP " + str(format(tmp, '.3f').zfill(6)) + str(TEMP_SCALE) + "- SW OFF"
             lcd.lcd_display_string(msg, 4)
             print color.OKGREEN + msg + color.ENDC
         #tmp >= tset just being off
-        elif tmp >= TSET and (STATE==False):
+        elif float(tmp) >= float(TSET) and (STATE==False):
             if count > 3:
                 sockets("off", YELLOW)
                 count = 0
-            msg = "TEMP " + str(tmp) + str(TEMP_SCALE) + "- OFF   "
+            msg = "TEMP " + str(format(tmp, '.3f').zfill(6)) + str(TEMP_SCALE) + "- OFF   "
             lcd.lcd_display_string(msg, 4)
             print color.OKGREEN + msg + color.ENDC
         #tmp<tset status is true and heating is running
-        elif tmp<TSET and (STATE==True):
+        elif float(tmp) < float(TSET) and (STATE==True):
             if count > 3:
                 sockets("on", YELLOW)
                 count = 0
-            msg = "TEMP " + str(tmp) + str(TEMP_SCALE) + "- ON    "
+            msg = "TEMP " + str(format(tmp, '.3f').zfill(6)) + str(TEMP_SCALE) + "- ON    "
             lcd.lcd_display_string(msg, 4)
             print color.BLUE + msg + color.ENDC
         #tmp<tset-tmarg switching on
-        elif tmp < TSET - TMARG and (STATE==None or not STATE or STATE==False):
+        elif float(tmp) < float(TSET) - float(TMARG) and (STATE==None or not STATE or STATE==False):
             STATE = True
             sockets("on", YELLOW)
-            msg = "TEMP " + str(tmp) + str(TEMP_SCALE) + "- SW ON "
+            msg = "TEMP " + str(format(tmp, '.3f').zfill(6)) + str(TEMP_SCALE) + "- SW ON "
             lcd.lcd_display_string(msg, 4)
             print color.BLUE + msg + color.ENDC
         #tset-tmarg<=tmp
-        elif tmp >=TSET-TMARG and STATE==False:
-            msg = "TEMP " + str(tmp) + str(TEMP_SCALE) + "- OFF   "
+        elif float(tmp) >= float(TSET) - float(TMARG) and STATE==False:
+            msg = "TEMP " + str(format(tmp, '.3f').zfill(6)) + str(TEMP_SCALE) + "- OFF   "
             lcd.lcd_display_string(msg, 4)
             print color.WARNING + msg + color.ENDC
         
         count = count + 1
         time.sleep(5)
 except KeyboardInterrupt:
+    sockets("off", YELLOW)
     msg = "   ** INACTIVE **   "
-    lcd.lcd_display_string(msg1, 4)
-    lcd.lcd_display_string(msg2, 4)
-    lcd.lcd_display_string(msg1, 4)
-    lcd.lcd_display_string(msg1, 4)
+    lcd.lcd_display_string(msg, 1)
+    lcd.lcd_display_string(msg, 2)
+    lcd.lcd_display_string(msg, 3)
+    lcd.lcd_display_string(msg, 4)
     GPIO.cleanup()
     print("Quitting...")
 except Exception as err:
+    sockets("off", YELLOW)
     msg = "   ** INACTIVE **   "
-    lcd.lcd_display_string(msg1, 4)
-    lcd.lcd_display_string(msg2, 4)
-    lcd.lcd_display_string(msg1, 4)
-    lcd.lcd_display_string(msg1, 4)
+    lcd.lcd_display_string(msg, 1)
+    lcd.lcd_display_string(msg, 2)
+    lcd.lcd_display_string(msg, 3)
+    lcd.lcd_display_string(msg, 4)
     GPIO.cleanup()
     print("Error: " + str(err))
     sys.exit(1)
